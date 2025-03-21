b09b801bfc585d927e07575b5671a0b91035dc15
Stenkross <Tandkrok@gmail.com>
2025-03-21 14:35:53 +0100

    Slideshows
    Har lagt till koden för slideshows i js och html sidan

--- js/js.js
+++ js/js.js
@@ -19,3 +19,27 @@ document.addEventListener("click", function (event) {
         transform(menuButton); 
     }
 });
+
+/* slideshows */
+let slideIndex = [1, 1, 1, 1];
+let slideId = ["slideshow_1", "slideshow_2", "slideshow_3", "slideshow_4"]
+showSlides(1, 0);
+showSlides(1, 1);
+showSlides(1, 2);
+showSlides(1, 3);
+
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+function showSlides(n, no) {
+  let i;
+  let x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex[no]-1].style.display = "block";
+}
